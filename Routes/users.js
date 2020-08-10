a5252ed594ccfa988434a33f2910970d2f9be997
sandeepadode <37178101+sandeepadode@users.noreply.github.com>
2020-08-10 00:35:45 -0400

    Added UpdatePassword API and also fixed the userid auto increment issue

--- Routes/users.js
+++ Routes/users.js
@@ -1,6 +1,7 @@
 // const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 // Importing User Schema 
 const User = require('../Models/User');
 const Mongoose = require('mongoose');
@@ -31,15 +32,17 @@ router.post('/login', async (req, res, next) => {
     });
 
     // Change password
-    router.patch('/Changepassword',async(req, res, next) =>{
-        let user = new User();
+    router.patch('/Changepassword',async(req, res, next) =>{        
      const checkUser = await User.findOne({Email: req.body.Email});
      if (checkUser != null) {
-        if (checkUser.validPassword(req.body.password)) { 
-            const query = {User_Id: checkUser.UserId}
-            // const update = {$set:setPassword(req.body.NewPassword)};
-            const updatePassword = await User.updateOne(query, checkUser.setPassword(req.body.NewPassword));
-            if (updatePassword != null) {
+        if (checkUser.validPassword(req.body.password)) {
+            const salt = crypto.randomBytes(16).toString('hex');
+            const hashedPassword = crypto.pbkdf2Sync(req.body.NewPassword, salt, 1000, 64, `sha512`).toString(`hex`); 
+            const query = {Email: req.body.Email}
+            const update = {$set:{hash: hashedPassword, salt: salt}};
+
+            const updatePassword = await User.updateOne(query,update);
+            if (updatePassword.n > 0) {
                 res.status(201).json({ 
                     message : "User Password Updated" 
                 });
@@ -71,29 +74,36 @@ router.post('/signup', async (req, res, next) => {
      
     // Creating empty user object 
        let newUser = new User(); 
-     
-       // Initialize newUser object with request data
-       newUser._id = new Mongoose.Types.ObjectId,
-       newUser.Name = req.body.Name,
-       newUser.Email = req.body.Email,
-       newUser.UserId = req.body.UserId,
-       newUser.DateOfBirth = req.body.DateOfBirth,
-       newUser.Phone = req.body.Phone,
-       newUser.DateJoined = Date.now(),
-       newUser.UpdatedDate = Date.now(),
-       newUser.Activated = req.body.Activated,
-       newUser.Role = req.body.Role,            
-     
-         // Call setPassword function to hash password 
-         newUser.setPassword(req.body.password); 
-     
-       // Save newUser object to database 
-       const savedUser = await newUser.save();
-       if (savedUser != null) {
-           res.json({message: "User added successfully", savedUser});
+       const checkUser = await User.findOne({Email: req.body.Email});
+       if (checkUser == null) {
+        const latestUser = await User.find().sort({'UserId':-1}).limit(1);
+        const newUserId = latestUser[0].UserId + 1; 
+        // Initialize newUser object with request data
+        newUser._id = new Mongoose.Types.ObjectId,
+        newUser.Name = req.body.Name,
+        newUser.Email = req.body.Email,
+        newUser.UserId = newUserId,
+        newUser.DateOfBirth = req.body.DateOfBirth,
+        newUser.Phone = req.body.Phone,
+        newUser.DateJoined = Date.now(),
+        newUser.UpdatedDate = Date.now(),
+        newUser.Activated = req.body.Activated,
+        newUser.Role = req.body.Role,            
+      
+          // Call setPassword function to hash password 
+          newUser.setPassword(req.body.password); 
+      
+        // Save newUser object to database 
+        const savedUser = await newUser.save();
+        if (savedUser != null) {
+            res.json({message: "User added successfully", savedUser});
+        } else {
+         res.json({message: "User could not be added successfully"});
+        }
        } else {
-        res.json({message: "User could not be added successfully"});
+           res.json({message:"User already exists"});
        }
+    
        
    }); 
    
